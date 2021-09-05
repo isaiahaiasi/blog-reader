57913e8e0381a94e48fe7f1fc57b197694974ce9
Isaiah <computerblender@gmail.com>
2021-09-05 16:24:42 -0700

    Simplify github link to remove scroll issues

--- src/components/GithubLink.tsx
+++ src/components/GithubLink.tsx
@@ -1,35 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { IconContext } from "react-icons/lib";
 
 // TODO: figure out how to properly style "footer" position for all screens
-const GithubLink = () => {
-  const [isHovering, setIsHovering] = useState(false);
-
-  return (
-    <div className="p-2 pb-6 overflow-hidden lg:pb-2 lg:overflow-visible">
-      <div
-        className="transform origin-top-left lg:origin-bottom-left hover:scale-125 transition"
-        onMouseEnter={() => setIsHovering(true)}
-        onMouseLeave={() => setIsHovering(false)}
+const GithubLink = () => (
+  <div className="p-2 pt-4 overflow-hidden">
+    <div className="transform origin-bottom-left hover:scale-125 transition">
+      <a
+        href="https://github.com/isaiahaiasi/blog-reader"
+        target="_blank"
+        rel="noreferrer noopener"
+        className=""
       >
-        <a
-          href="https://github.com/isaiahaiasi/blog-reader"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="flex items-center lg:flex-col-reverse lg:items-baseline"
-        >
-          <IconContext.Provider value={{ color: "white", size: "3rem" }}>
-            <FaGithub />
-          </IconContext.Provider>
-
-          {isHovering && (
-            <p className="p-1 text-white align-middle text-sm">See code!</p>
-          )}
-        </a>
-      </div>
+        <IconContext.Provider value={{ color: "white", size: "3rem" }}>
+          <FaGithub />
+        </IconContext.Provider>
+      </a>
     </div>
-  );
-};
+  </div>
+);
 
 export default GithubLink;

654628270ba3d4c0186902e2b0a668721c198cfd
Isaiah <computerblender@gmail.com>
2021-09-05 18:17:10 -0700

    >Update routes to reflect server-side changes

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -2,16 +2,16 @@ import { API_HOST_NAME } from "./constants";
 
 //* API ROUTES
 const API_ROOT = `${API_HOST_NAME}/api`;
-export const API_USER = `${API_ROOT}/user`;
-export const API_BLOGPOST = `${API_ROOT}/blog`;
+export const API_USER = `${API_ROOT}/users`;
+export const API_BLOGPOST = `${API_ROOT}/blogs`;
 export const API_LOGIN = `${API_ROOT}/auth/login`;
-export const API_REGISTER = `${API_ROOT}/user`;
-export const API_COMMENT = `${API_ROOT}/comment`;
+export const API_REGISTER = `${API_ROOT}/users`;
+export const API_COMMENT = `${API_ROOT}/comments`;
 
-export const getUserBlog = (userid: string) => `${API_USER}/${userid}/blog`;
+export const getUserBlog = (userid: string) => `${API_USER}/${userid}/blogs`;
 export const getPost = (blogid: string) => `${API_BLOGPOST}/${blogid}`;
 export const getPostComments = (blogid: string) =>
-  `${API_BLOGPOST}/${blogid}/comment`;
+  `${API_BLOGPOST}/${blogid}/comments`;
 export const getComment = (commentid: string) => `${API_COMMENT}/${commentid}`;
 
 //* PAGE ROUTES
